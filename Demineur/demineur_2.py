b36af98f7702d9c479cd388b148a4ce695a0fa22
daphnexmn-prog <daphne.xmn@gmail.com>
2025-12-23 13:48:23 +0100

    Commentaires

--- Demineur/demineur_2.py
+++ Demineur/demineur_2.py
@@ -1,28 +1,38 @@
 import tkinter as tk
 from random import randint
 
-board = tk.Tk()
-board.geometry("930x520")
-
-SIZE_X = 30
-SIZE_Y = 20
-NB_MINES = 100
-
-#coucou
+SIZE_X = 30 # nombre de colonnes
+SIZE_Y = 20 # nombre de lignes
+NB_MINES = 100 # nombre de mines
 
 def create_board():
-    mines=0
-    tiles=[[[0] for i in range (SIZE_X)] for j in range (SIZE_Y)]
-    for n in range (NB_MINES):
-        while mines<NB_MINES:
-            randx=randint(0,SIZE_X-1)
-            randy=randint(0,SIZE_Y-1)
-            if tiles[randy][randx]==[0]:
-                tiles[randy][randx]=[1]
-                mines+=1
+    """Crée la grille avec NB_MINES mines placées aléatoirement
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    ----------
+    list
+        La grille remplie de [0] (case vide) et de [1] (bombe)
+    """
+    mines = 0 # compteur de mines 
+    tiles = [[[0] for i in range (SIZE_X)] for j in range (SIZE_Y)] # remplit la grille de [0]
+ 
+    while mines < NB_MINES:
+        randx = randint(0,SIZE_X-1)
+        randy = randint(0,SIZE_Y-1)
+        if tiles[randy][randx] == [0]:
+            tiles[randy][randx] = [1]
+            mines += 1
+        
     return tiles
+print(create_board())
 
-
+board = tk.Tk()
+board.geometry("930x520")
+board.title("Démineur")
 for col in range(SIZE_X):
     for row in range(SIZE_Y):
         bouton = tk.Button(board, width=3)
