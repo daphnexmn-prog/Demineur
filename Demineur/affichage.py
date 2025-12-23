156ea70e24df8a93662606e774e6d91b10fdfcbe
daphnexmn-prog <daphne.xmn@gmail.com>
2025-12-23 16:51:45 +0100

    Commmentaires suite

--- Demineur/affichage.py
+++ Demineur/affichage.py
@@ -1,9 +1,15 @@
+"""
+Affichage 
+"""
+
 from fonctions import *
 
-board = tk.Tk()
+board = tk.Tk() # création de la fenêtre
 board.geometry("930x520")
 board.title("Démineur")
 for col in range(SIZE_X):
     for row in range(SIZE_Y):
         bouton = tk.Button(board, width=3)
-        bouton.grid(row = row, column = col)
+        bouton.grid(row = row, column = col) # place chaque bouton
+board.mainloop()
+print(help(create_board))
--- Demineur/fonctions.py
+++ Demineur/fonctions.py
@@ -1,3 +1,8 @@
+"""
+Fonctions :
+    create_board() : Crée la grille avec NB_MINES placées aléatoirement
+"""
+
 import tkinter as tk
 from random import randint
 
@@ -6,25 +11,24 @@ SIZE_Y = 20 # nombre de lignes
 NB_MINES = 100 # nombre de mines
 
 def create_board():
-    """Crée la grille avec NB_MINES mines placées aléatoirement
+    """
+    Crée la grille avec NB_MINES mines placées aléatoirement
 
     Parameters
     ----------
     None
 
     Returns
-    ----------
-    list
+    -------
+    list[list[list[int]]]
         La grille remplie de [0] (case vide) et de [1] (bombe)
     """
     mines = 0 # compteur de mines 
     tiles = [[[0] for i in range (SIZE_X)] for j in range (SIZE_Y)] # remplit la grille de [0]
- 
     while mines < NB_MINES:
-        randx = randint(0,SIZE_X-1)
-        randy = randint(0,SIZE_Y-1)
+        randx = randint(0,SIZE_X-1) # colonne aléatoire
+        randy = randint(0,SIZE_Y-1) # ligne aléatoire
         if tiles[randy][randx] == [0]:
             tiles[randy][randx] = [1]
             mines += 1
-        
     return tiles
